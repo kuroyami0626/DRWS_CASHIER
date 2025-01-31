51fe2e2a994da0b5b71d823ca942950fc083362b
tel17 <chestermatanguihan19@gmail.com>
2025-01-31 16:17:39 +0800

    as

--- members_profile.php
+++ members_profile.php
@@ -228,38 +228,7 @@
                 <label for="family_member_3" class="form-label" style="display: none;">FAMILY MEMBER 3:</label>
                 <input type="text" id="family_member_3" name="family_member_3"class="form-input" placeholder="Enter your family member" style="display: none;">
             </div>
-            <!-- <div class="form-group">
-                <label for="famMem4" class="form-label" style="display: none;">FAMILY MEMBER 4:</label>
-                <input type="text" id="famMem4" class="form-input" placeholder="Enter your family member" style="display: none;">
-            </div>
-            <div class="form-group">
-                <label for="famMem5" class="form-label" style="display: none;">FAMILY MEMBER 5:</label>
-                <input type="text" id="famMem5" class="form-input" placeholder="Enter your family member" style="display: none;">
-            </div>
-            <div class="form-group">
-                <label for="famMem6" class="form-label" style="display: none;">FAMILY MEMBER 6:</label>
-                <input type="text" id="famMem6" class="form-input" placeholder="Enter your family member" style="display: none;">
-            </div>
-            <div class="form-group">
-                <label for="famMem7" class="form-label" style="display: none;">FAMILY MEMBER 7:</label>
-                <input type="text" id="famMem7" class="form-input" placeholder="Enter your family member" style="display: none;">
-            </div>
-            <div class="form-group">
-                <label for="famMem8" class="form-label" style="display: none;">FAMILY MEMBER 8:</label>
-                <input type="text" id="famMem8" class="form-input" placeholder="Enter your family member" style="display: none;">
-            </div>
-            <div class="form-group">
-                <label for="famMem9" class="form-label" style="display: none;">FAMILY MEMBER 9:</label>
-                <input type="text" id="famMem9" class="form-input" placeholder="Enter your family member" style="display: none;">
-            </div>
-           
-            <div class="form-group">
-                <label for="famMem10" class="form-label" style="display: none;">FAMILY MEMBER 10:</label>
-                <input type="text" id="famMem10" class="form-input" placeholder="Enter your family member" style="display: none; ">
-               
-            </div> -->
-                    <!-- <button class="btn btn-primary" id="add-btn" >Add Family Member</button>
-                    <button class="btn btn-danger" id="delete-btn" >Delete Family Member</button> -->
+         
            
         </div>
  
@@ -297,12 +266,19 @@
                 <label for="month" class="form-label">MONTH:</label>
                 <select  id="month" name="month" class="form-control">
                     <option value="" disabled selected>Choose a month</option>
-                    <?php
-                    $months = array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
-                    foreach ($months as $month) {
-                        echo '<option value="' . $month . '">' . $month . '</option>';
-                    }
-                    ?>
+                    <option value="january">january</option>
+                    <option value="february">february</option>
+                    <option value="march">march</option>
+                    <option value="april">april</option>
+                    <option value="may">may</option>
+                    <option value="june">june</option>
+                    <option value="july">july</option>
+                    <option value="august">august</option>
+                    <option value="september">september</option>
+                    <option value="october">october</option>
+                    <option value="november">november</option>
+                    <option value="december">december</option>
+
                 </select>
             </div>
             <div class="form-group">
